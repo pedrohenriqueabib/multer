f4a005557dba4522c4980624d37d5ac758d581fd
pedrohenriqueabib <pedrohenriqueabib@gmail.com>
2021-07-02 19:28:18 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now()+'-'+file.originalname)
+    }
+});
+const upload = multer({storage});
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('home'));
+
+app.post('/', upload.single('img'), (req, res) =>{
+    console.log(req.body, req.file);
+    res.send('ok');
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log('running....'));
