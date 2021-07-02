4f11595d13547f49a10edd196b44dc05d9dcfabf
pedrohenriqueabib <pedrohenriqueabib@gmail.com>
2021-07-02 19:40:35 -0300

    teste1 package.json

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.post('/', upload.single('img'), (req, res) =>{
     res.send('ok');
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('running....'));
